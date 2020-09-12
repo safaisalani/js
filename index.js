9f8e88406723d68c3f7ed38e87d3c65ba541a8c5
safaisalani <59282875+safaisalani@users.noreply.github.com>
2020-09-12 09:43:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+  alert("Enter your name and press done");
+   //function r(){
+         //var x = document.getElementsById(&#39;fname&#39;).value;
+        //alert(x);
+           //var x = document.getElementsById(&quot;fname&quot;).value;
+ //document.write(x);
+  //x[0].innerHTML = &quot;Hello World!&quot;;}
+  
+  
+          function getInputValue(){
+
+            // Selecting the input element and get its value
+
+            var inputVal = document.getElementById("myInput").value;
+        if (inputVal == 7){ window.open(";", "_blank"); }
+
+        
+
+            
+
+ 
+ 
+        //document.write(typeof(inputVal));
+        //document.write(inputVal);
+
+        }
+  
+ 
+  
+            function get(){
+
+            // Selecting the input element and get its value 
+
+            var inputVal = document.getElementById("myInp").value;
+        
+
+            
+
+ 
+            alert("Hi "+inputVal+" Enter a number and press Finish");
+        //document.write(typeof(inputVal));
+        //document.write(inputVal);
+
+        }
